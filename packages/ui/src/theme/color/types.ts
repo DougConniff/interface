c9d8c641920df399b5cca821bfae9c60a85ffb6c
Launch Stream DEX <launchstream.xyz@gmail.com>
2025-07-27 20:19:31 -0700

    Update types.ts

--- packages/ui/src/theme/color/types.ts
+++ packages/ui/src/theme/color/types.ts
@@ -1,5 +1,5 @@
-import { colors, colorsLight } from 'ui/src/theme/color/colors'
+import { colors, colorsDark } from 'ui/src/theme/color/colors'
 
 export type GlobalPalette = typeof colors
 export type GlobalColorNames = keyof GlobalPalette
-export type Palette = typeof colorsLight
+export type Palette = typeof colorsDark
